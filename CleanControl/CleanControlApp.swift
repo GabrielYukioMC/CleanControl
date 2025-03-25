4834557f9a3d02d64db911d1ad877663c6eb25ed
Gabriel Yukio <gabriel.coca@sptech.school>
2025-03-25 20:21:09 -0300

    Initial Commit

---
+++ CleanControl/CleanControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanControlApp.swift
+//  CleanControl
+//
+//  Created by Gabriel Yukio on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanControlTests/CleanControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanControlTests.swift
+//  CleanControlTests
+//
+//  Created by Gabriel Yukio on 25/03/25.
+//
+
+import Testing
+@testable import CleanControl
+
+struct CleanControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
